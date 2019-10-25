7f7192c6855be4afb105feb78d117f5dcf970a28
Alexander Skrypnyk <alexanderlookua@gmail.com>
2019-10-25 13:20:46 +0300

    Initial Commit

---
+++ SimpleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleApp
+//
+//  Created by Alexander Skrypnyk on 10/25/19.
+//  Copyright © 2019 Alexander Skrypnyk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
