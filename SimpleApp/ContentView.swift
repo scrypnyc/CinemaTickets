3279795a5bf81d41a6c28461877b1a7be66b42a7
Alexander Skrypnyk <alexanderlookua@gmail.com>
2019-10-30 00:38:36 +0200

    Added changes

--- SimpleApp/ContentView.swift
+++ SimpleApp/ContentView.swift
@@ -20,51 +20,55 @@ struct ContentView: View {
     
     let movies: [[Movie]] = [
         [.init(name: "Django", imageName: "Django"),
-         .init(name: "Fight Club", imageName: "Club")],
-        [.init(name: "Call Me Your Name", imageName: "CallMe"),
-         .init(name: "Blade Runner", imageName: "Blade")],
-        [.init(name: "Django", imageName: "Django"),
-         .init(name: "Fight Club", imageName: "Club")],
-        [.init(name: "Call Me Your Name", imageName: "CallMe"),
+         .init(name: "Dunkirk", imageName: "Dunkirk")],
+        [.init(name: "John Wick", imageName: "John"),
+         .init(name: "Moonlight", imageName: "Moonlight")],
+        [.init(name: "Killing Them Softly", imageName: "KillingThemSoftly"),
+         .init(name: "Call Me Your Name", imageName: "CallMe")],
+        [.init(name: "Fight Club", imageName: "Club"),
          .init(name: "Blade Runner", imageName: "Blade")]
     ]
     
     
     var body: some View {
-        ScrollView {
-            VStack  (spacing: 15){
-                Text("CinemaTickets")
-                    .font(.system(size: 23, weight: .bold))
-                ForEach(movies, id: \.self) { row in
-                    HStack {
-                        ForEach (row) { movie in
-                            VStack (spacing: 4) {
-                                Image(movie.imageName)
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 170, height: 250)
-                                    .clipped()
-                                    .cornerRadius(10)
-                                Text(movie.name)
-                                Button(action: {
-                                    
-                                }) {
-                                    Text("Buy Tickets")
-                                        .padding(.horizontal, 40)
-                                        .padding(.vertical, 6)
-                                        .background(Color.red)
-                                        .foregroundColor(Color.white)
-                                    .cornerRadius(10)
+        GeometryReader { geo in
+            ScrollView {
+                VStack  (spacing: 45){
+                    Text("CinemaTickets")
+                        .font(.system(size: 23, weight: .semibold))
+                    ForEach(self.movies, id: \.self) { row in
+                        HStack (spacing: 15) {
+                            ForEach (row) { movie in
+                                VStack (spacing: 4) {
+                                    Image(movie.imageName)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: (geo.size.width - 48) / 2, height: 250)
+                                        .clipped()
+                                        .cornerRadius(10)
+                                        .shadow(radius: 10)
+                                    Text(movie.name)
+                                        .font(.system(size: 14, weight: .bold))
+                                    Button(action: {
+                                        
+                                    }) {
+                                        Text("Buy Tickets")
+                                            .padding(.horizontal, 40)
+                                            .padding(.vertical, 6)
+                                            .background(Color.red)
+                                            .foregroundColor(Color.white)
+                                        .cornerRadius(10)
+                                        .shadow(radius: 10)
+                                    }
                                 }
                             }
                         }
                     }
-
+                    Spacer()
                 }
-                Spacer()
             }
         }
-        .padding(.all, 15)
+
        
     }
 }
